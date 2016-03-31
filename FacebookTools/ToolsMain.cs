6b5673544524926251d34d37cdf1c2bacdcbe582
rangliks <rangliks@gmail.com>
2016-04-01 01:26:50 +0300

    OOps

--- FacebookTools/ToolsMain.cs
+++ FacebookTools/ToolsMain.cs
@@ -10,7 +10,7 @@ namespace FacebookTools
     {
         static void Main(string[] args)
         {
-            FacebookHelper helper = new FacebookHelper("CAAC3FZAauWzEBAP7FQAZBT0P2m5zIL1ImfyRcgOy1qJWIanCyhmiD1GszN0Fk0gI4ZBL2bOaxIGln3NlrlzPnZA3Nchfy0xdfSSumgBbfQLaRSOTtl4I4LZCVxh4zG0ETOrLYAsFRZBOwdUyycmAbRrs66AGghNVN9FL2C761UXUrGmZBdnzsGmG4IOD5K5tul3vjNZA6qm8pwZDZD");
+            FacebookHelper helper = new FacebookHelper("");
             helper.GetFamily();
         }
     }
